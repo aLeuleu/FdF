86444fdad060fd7fb0e659cadd61a1650f87efce
aLeuleu <aurelien.levra@gmail.com>
2023-01-12 00:09:20 +0100

    draw_lines_between_pts

--- src/fdf.c
+++ src/fdf.c
@@ -6,13 +6,13 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 04:09:20 by bajeanno          #+#    #+#             */
-/*   Updated: 2023/01/11 23:50:17 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2023/01/12 00:06:13 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static void	display_line(t_map *map, int line, t_win *win, int line_pos, t_p spacing);
+static void	display_line(t_map *map, int line, t_win *win, t_p spacing);
 static void	display_map(t_map *map, t_win *win);
 static t_win	*win_init(int height, int width, char *window_title);
 static	void	put_pixel(int x, int y, t_win *win);
@@ -33,7 +33,7 @@ int	main(int argc, char **argv)
 
 	map = get_map(argv[1]);
 	
-	win = win_init(1000, 1500, "fdf");
+	win = win_init(800, 1200, "fdf");
 	if (!win)
 		return (-1);//...
 	// print_map(map);
@@ -85,30 +85,37 @@ static void	display_map(t_map *map, t_win *win)
 	i = 0;
 	while (i < map->height)
 	{
-		display_line(map, i, win, (i)*spacing.y, spacing);
+		display_line(map, i, win, spacing);
 		i ++;
 	}
 }
 
 
-static void		display_line(t_map *map, int line, t_win *win, int line_pos, t_p spacing)
+static void		display_line(t_map *map, int line, t_win *win, t_p spacing)
 {
 	int	i;
-	int	j;
 	t_p	offset;
 	t_p	p;
-	
-	
+	int	k;
+	int	line_pos;
+
+	line_pos = line * spacing.y;
 	offset.x = spacing.x /2; //(win->width)/2  - ((map->width - 1) * spacing_x)/2 ; //   
 	offset.y = spacing.y /2;// (win->height)/2 - ((map->height - 1) * spacing_y)/2;//  
-	j = line_pos;
 	i = 0;
 	while (i < map->width)
 	{
 		p.x = (i * spacing.x) + offset.x;
-		p.y = j + offset.y;
-		
-		put_pixel(p.x, p.y, win);
+		p.y = line_pos + offset.y;
+		k = 0;
+		if (i != map->width -1)
+			while (k < spacing.x)
+				put_pixel(p.x + k++, p.y, win);
+		k = 0;
+		if (line != map->height -1)
+			while (k < spacing.y)
+				put_pixel(p.x , p.y + k++, win);
+
 		i++;	
 	}
 }
