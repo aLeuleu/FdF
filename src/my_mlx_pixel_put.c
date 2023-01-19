09c1d672d792090927d8e0ee541416180d237f3b
aLeuleu <aurelien.levra@gmail.com>
2023-01-19 23:15:20 +0100

    optimisation bit by pixel

--- src/my_mlx_pixel_put.c
+++ src/my_mlx_pixel_put.c
@@ -6,7 +6,7 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 18:40:05 by alevra            #+#    #+#             */
-/*   Updated: 2023/01/18 18:48:09 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2023/01/19 23:14:01 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = data->addr + (y * data->line_length + x * data->bits_per_pixel);
 	*(unsigned int *)dst = color;
 }
