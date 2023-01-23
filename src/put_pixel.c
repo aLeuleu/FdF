0f79a583840c09e40692b79c2ad81854ca120d96
aLeuleu <aurelien.levra@gmail.com>
2023-01-23 14:59:01 +0100

    fix bus error (negative points)

--- src/put_pixel.c
+++ src/put_pixel.c
@@ -6,7 +6,7 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 16:22:38 by alevra            #+#    #+#             */
-/*   Updated: 2023/01/20 00:24:10 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2023/01/23 14:58:40 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	put_pixel(t_p p, t_win *win, t_data *data, int color)
 {
 	char	*dst;
 
-	if (p.x < win->width && p.y < win->height)
+	if (p.x >= 0 && p.y >= 0 && p.x < win->width && p.y < win->height)
 	{
 		dst = data->addr + \
 		(p.y * data->line_length + p.x * data->bits_per_pixel);
